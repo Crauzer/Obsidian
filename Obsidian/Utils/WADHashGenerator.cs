e42235bd2b27ea281dfb5a5bac9dea925f20c8fb
Crauzer <filip.quitko@gmail.com>
2018-02-20 22:24:50 +0100

    changes

--- Obsidian/Utils/WADHashGenerator.cs
+++ Obsidian/Utils/WADHashGenerator.cs
@@ -15,67 +15,7 @@ namespace Obsidian.Utils
     {
         public static void GenerateWADStrings(ILog logger, WADFile wad, Dictionary<ulong, string> stringDictionary)
         {
-            foreach (WADEntry wadEntry in wad.Entries.Where(x => x.Type == EntryType.Compressed || x.Type == EntryType.ZStandardCompressed))
-            {
-                byte[] entryData = wadEntry.GetContent(true);
-                if (Utilities.GetLeagueFileExtensionType(entryData) == LeagueFileType.BIN)
-                {
-                    List<string> wadEntryStrings = new List<string>();
-                    BINFile bin = null;
 
-                    try
-                    {
-                        bin = new BINFile(new MemoryStream(entryData));
-                        
-                    }
-                    catch
-                    {
-                        logger.Warn("Loading Dictionary Strings from WAD Entry: " 
-                            +  BitConverter.ToString(BitConverter.GetBytes(wadEntry.XXHash)).Replace("-", "") 
-                            + " failed");
-                        continue;
-                    }
-
-                    wadEntryStrings.AddRange(GetLinkedFileStrings(bin.LinkedFiles).Distinct());
-
-                    foreach (BINFileEntry binEntry in bin.Entries)
-                    {
-                        foreach (BINFileValue binValue in binEntry.Values.Where(x =>
-                        x.Type == BINFileValueType.String || x.Value.GetType() == typeof(BINFileValueList)))
-                        {
-                            switch (binValue.Type)
-                            {
-                                case BINFileValueType.String:
-                                    wadEntryStrings.Add(binValue.Value as string);
-                                    break;
-                                case BINFileValueType.PairList:
-                                case BINFileValueType.LargeStaticTypeList:
-                                case BINFileValueType.List:
-                                case BINFileValueType.List2:
-                                case BINFileValueType.SmallStaticTypeList:
-                                    wadEntryStrings.AddRange(GetValueStrings(binValue));
-                                    break;
-                            }
-                        }
-                    }
-
-                    using (XXHash64 xxHash = XXHash64.Create())
-                    {
-                        wadEntryStrings.ForEach(x =>
-                        {
-                            if (x != "")
-                            {
-                                string loweredName = x.ToLower();
-                                ulong hash = BitConverter.ToUInt64(xxHash.ComputeHash(Encoding.ASCII.GetBytes(loweredName)), 0);
-                                if (!stringDictionary.ContainsKey(hash))
-                                {
-                                    stringDictionary.Add(hash, x);
-                                }
-                            }
-                        });
-                    }
-                }
-            }
         }
 
         private static IEnumerable<string> GetLinkedFileStrings(List<string> linkedFiles)
@@ -92,7 +32,7 @@ namespace Obsidian.Utils
                     continue;
                 }
 
-                string[] strings = linkedFile.Split(new char[] {'_'}, StringSplitOptions.RemoveEmptyEntries);
+                string[] strings = linkedFile.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
                 characterNames.Add(strings[0].Remove(0, 5));
 
                 string extension = strings[strings.Length - 1].Substring(strings[strings.Length - 1].IndexOf(".", StringComparison.Ordinal));
@@ -110,44 +50,6 @@ namespace Obsidian.Utils
                     yield return string.Format("DATA/Characters/{0}/Skins/Root{1}", characterNames[i], extension);
                 }
             }
-        }
-
-        private static IEnumerable<string> GetValueStrings(BINFileValue value)
-        {
-            List<string> strings = new List<string>();
-            if (value.Type == BINFileValueType.String)
-            {
-                strings.Add(value.Value as string);
-            }
-            else
-            {
-                if (value.Value is BINFileValueList valueList)
-                {
-                    foreach (BINFileValue binValue in valueList.Entries.Where(x => x is BINFileValue))
-                    {
-                        switch (binValue.Type)
-                        {
-                            case BINFileValueType.String:
-                                strings.Add(binValue.Value as string);
-                                break;
-                            case BINFileValueType.PairList:
-                            case BINFileValueType.LargeStaticTypeList:
-                            case BINFileValueType.List:
-                            case BINFileValueType.List2:
-                            case BINFileValueType.SmallStaticTypeList:
-                                foreach (BINFileValue binValue2 in ((BINFileValueList)binValue.Value).Entries.Where(x =>
-                                    x is BINFileValue &&
-                                    ((BINFileValue)x).Type == BINFileValueType.String))
-                                {
-                                    strings.AddRange(GetValueStrings(binValue2));
-                                }
-                                break;
-                        }
-                    }
-                }
-            }
-
-            return strings.AsEnumerable();
         }
     }
 }
