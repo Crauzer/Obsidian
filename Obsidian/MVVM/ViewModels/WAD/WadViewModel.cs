f8eda9326e2b695217141d48473d4b0c9e2530a4
Crauzer <filip.quitko@gmail.com>
2020-08-13 19:58:45 +0200

    i like noodles

--- Obsidian/MVVM/ViewModels/WAD/WadViewModel.cs
+++ Obsidian/MVVM/ViewModels/WAD/WadViewModel.cs
@@ -66,7 +66,7 @@ namespace Obsidian.MVVM.ViewModels.WAD
                                 }
                             }
                             default:
-                                throw new Exception("this should not happen :^)");
+                                return false;
                         }
                     }
                     catch(Exception)
