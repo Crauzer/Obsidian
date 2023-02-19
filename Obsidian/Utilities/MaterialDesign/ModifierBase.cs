15e11ab7f599d0c8df9ee9294e0783fece36bd8e
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-02-19 21:49:22 +0100

    remove garbage

--- Obsidian/Utilities/MaterialDesign/ModifierBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace Obsidian.Utilities.MaterialDesign
-{
-    public abstract class ModifierBase
-    {
-        public abstract void Apply(DependencyObject target);
-    }
-}
