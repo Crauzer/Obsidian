bb29375cb5cc60e710f32bb19d497d6884627f4a
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-02-19 22:16:43 +0100

    add non-garbage

---
+++ Obsidian/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
+using MudBlazor.Services;
+using Photino.Blazor;
+
+namespace Obsidian;
+
+public class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        PhotinoBlazorAppBuilder builder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+        builder.Services.AddLogging();
+
+        // register root component and selector
+        builder.RootComponents.Add<App>("app");
+
+        builder.Services.AddMudServices(config =>
+        {
+            config.SnackbarConfiguration.PreventDuplicates = true;
+            config.SnackbarConfiguration.ShowCloseIcon = true;
+            config.SnackbarConfiguration.VisibleStateDuration = 5000;
+            config.SnackbarConfiguration.ShowTransitionDuration = 250;
+            config.SnackbarConfiguration.HideTransitionDuration = 250;
+            config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+            config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+        });
+
+        PhotinoBlazorApp app = builder.Build();
+
+        // customize window
+        app.MainWindow.UseOsDefaultSize = false;
+        app.MainWindow.SetIconFile("favicon.ico").SetTitle("Obsidian").Center().SetWidth(1600).SetHeight(1100);
+
+#if DEBUG
+        app.MainWindow.SetDevToolsEnabled(true);
+#endif
+
+        AppDomain.CurrentDomain.UnhandledException += (sender, error) =>
+        {
+            app.MainWindow.OpenAlertWindow("Fatal exception", error.ExceptionObject.ToString());
+        };
+
+        app.Run();
+    }
+}
