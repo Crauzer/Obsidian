b6e6168a8809a1b23c143231528a7c5513d7a8a5
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-02-22 00:09:19 +0100

    refine some styling

---
+++ Obsidian/Data/CustomIcons.cs
@@ -0,0 +1,6 @@
+﻿namespace Obsidian.Data;
+
+public static class CustomIcons
+{
+    public static readonly string Regex = @"<path xmlns=""http://www.w3.org/2000/svg"" fill-rule=""evenodd"" clip-rule=""evenodd"" d=""M10.012 2h.976v3.113l2.56-1.557.486.885L11.47 6l2.564 1.559-.485.885-2.561-1.557V10h-.976V6.887l-2.56 1.557-.486-.885L9.53 6 6.966 4.441l.485-.885 2.561 1.557V2zM2 10h4v4H2v-4z""/>";
+}
