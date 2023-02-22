a8c59852fedd655e71b809e3b5dacd20d35f71fc
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-02-22 18:30:38 +0100

    improve tab close button

---
+++ Obsidian/AppTheme.cs
@@ -0,0 +1,28 @@
+﻿using MudBlazor;
+
+namespace Obsidian;
+
+public static class AppTheme
+{
+    public static readonly MudTheme Theme =
+        new()
+        {
+            Palette = new Palette()
+            {
+                Primary = Colors.Red.Accent2,
+                AppbarBackground = "#712b36",
+                DrawerBackground = "#712b36"
+            },
+            PaletteDark = new PaletteDark()
+            {
+                Primary = Colors.Red.Accent2,
+                AppbarBackground = "#712b36",
+                DrawerBackground = "#712b36"
+            },
+            LayoutProperties = new LayoutProperties() { DefaultBorderRadius = "8px" },
+            Typography = new Typography()
+            {
+                Default = new Default() { FontFamily = new[] { "Fira Sans" } }
+            }
+        };
+}
