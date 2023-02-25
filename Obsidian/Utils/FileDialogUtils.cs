c2157b445f29b6edec36d70a24341b0b26469db3
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-02-25 20:45:34 +0100

    implement saving

--- Obsidian/Utils/FileDialogUtils.cs
+++ Obsidian/Utils/FileDialogUtils.cs
@@ -8,4 +8,10 @@ public static class FileDialogUtils
 {
 
     public static CommonFileDialogFilter CreateWadFilter() => new("Wad Archive", "wad,client,server");
+
+    public static IEnumerable<CommonFileDialogFilter> CreateGltfFilters()
+    {
+        yield return new("glTF Binary", "glb");
+        yield return new("glTF", "gltf");
+    }
 }
