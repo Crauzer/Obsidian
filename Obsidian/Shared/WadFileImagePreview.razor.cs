de32273b02285ba3772234a53db0752d5753b448
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-02-25 18:55:09 +0100

    Implement Save as PNG for Image preview

---
+++ Obsidian/Shared/WadFileImagePreview.razor.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.HighPerformance;
+using Microsoft.AspNetCore.Components;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using MudBlazor;
+using Obsidian.Data.Wad;
+using Obsidian.Utils;
+using PhotinoNET;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace Obsidian.Shared;
+
+public partial class WadFileImagePreview
+{
+    [Inject] public PhotinoWindow Window { get; set; }
+    [Inject] public ISnackbar Snackbar { get; set; }
+
+    [Parameter]
+    public WadTabModel WadTab { get; set; }
+
+    [Parameter]
+    public EventCallback OnSavingAsPng { get; set; }
+
+    private bool _isEnabled => this.WadTab.CurrentPreviewType is WadFilePreviewType.Image;
+
+    private bool _isSavingAsPng;
+
+    private void SaveAsPng()
+    {
+        CommonOpenFileDialog dialog = new() { DefaultFileName = this.WadTab.SelectedFile.Name };
+        dialog.Filters.Add(new("Image", "png"));
+
+        if (dialog.ShowDialog(this.Window.WindowHandle) is not CommonFileDialogResult.Ok)
+            return;
+
+        ToggleIsSavingAsPng(true);
+        try
+        {
+            using Stream fileStream = this.WadTab.Wad.LoadChunkDecompressed(this.WadTab.SelectedFile.Chunk).AsStream();
+            Image<Rgba32> image = ImageUtils.GetImageFromStream(fileStream);
+
+            image.SaveAsPng(dialog.FileName);
+
+            this.Snackbar.Add($"Saved {this.WadTab.SelectedFile.Name} as PNG!", Severity.Success);
+        }
+        catch (Exception exception)
+        {
+            SnackbarUtils.ShowHardError(this.Snackbar, exception);
+        }
+        finally
+        {
+            ToggleIsSavingAsPng(false);
+        }
+    }
+
+
+
+    private void ToggleIsSavingAsPng(bool value)
+    {
+        this._isSavingAsPng = value;
+        StateHasChanged();
+    }
+}
