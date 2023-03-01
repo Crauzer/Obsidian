30ab9f4b8517ea4b6926faadcb0c8ce62a5a1a1d
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-03-01 23:11:19 +0100

    refactor stuff I guess

--- Obsidian/Data/Wad/WadTabModel.cs
+++ Obsidian/Data/Wad/WadTabModel.cs
@@ -169,8 +169,6 @@ public class WadTabModel : IDisposable
         }
     }
 
-
-    public string GetViewportCanvasId() => $"{this.Id}_renderCanvas";
     public string GetViewportContainerId() => $"{this.Id}_viewport_container";
 
     public void Dispose()
