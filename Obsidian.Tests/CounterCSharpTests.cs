1b52b45c74c8d31eb503055171f547855bfef85d
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-03-11 14:57:53 +0100

    add template test project

---
+++ Obsidian.Tests/CounterCSharpTests.cs
@@ -0,0 +1,32 @@
+namespace Obsidian.Tests
+{
+    /// <summary>
+    /// These tests are written entirely in C#.
+    /// Learn more at https://bunit.dev/docs/getting-started/writing-tests.html#creating-basic-tests-in-cs-files
+    /// </summary>
+    public class CounterCSharpTests : TestContext
+    {
+        [Fact]
+        public void CounterStartsAtZero()
+        {
+            // Arrange
+            var cut = RenderComponent<Counter>();
+
+            // Assert that content of the paragraph shows counter at zero
+            cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
+        }
+
+        [Fact]
+        public void ClickingButtonIncrementsCounter()
+        {
+            // Arrange
+            var cut = RenderComponent<Counter>();
+
+            // Act - click button to increment counter
+            cut.Find("button").Click();
+
+            // Assert that the counter was incremented
+            cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+        }
+    }
+}
