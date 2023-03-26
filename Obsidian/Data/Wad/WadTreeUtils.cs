5908e7ebbc735f683bda3b0f47be641eb327754d
Crauzer <18646077+Crauzer@users.noreply.github.com>
2023-03-26 23:15:01 +0200

    Delete WadTreeUtils.cs

--- Obsidian/Data/Wad/WadTreeUtils.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Obsidian.Data.Wad;
-
-public static class WadTreeUtils
-{
-    public static string ComposeWadTreeItemPathComponents(IEnumerable<string> pathComponents) =>
-        string.Join('/', pathComponents);
-}
