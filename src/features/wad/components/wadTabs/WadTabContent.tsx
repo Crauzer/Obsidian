1a8143909fc9dae371ac1abc28f038dbf5158985
Filip Quitko <filip.quitko@gmail.com>
2023-12-09 13:32:22 +0100

    fix toolbar border

--- src/features/wad/components/wadTabs/WadTabContent.tsx
+++ src/features/wad/components/wadTabs/WadTabContent.tsx
@@ -69,8 +69,8 @@ const WadTabContent: React.FC<WadTabContentProps> = ({ wadId, items, pathCompone
   return (
     <div className="flex h-full flex-col gap-2">
       <div className="flex h-full flex-col rounded border border-gray-600 bg-gray-900">
-        <div className="flex flex-row rounded border border-gray-600 bg-gray-800">
-          <WadTabToolbar className="w-1/2 rounded" />
+        <div className="flex flex-row border-b border-gray-600 bg-gray-800">
+          <WadTabToolbar className="w-1/2" />
           <Input className="m-1 w-1/2 flex-1" />
         </div>
         <Breadcrumbs.Root className="border-b border-gray-600 bg-gray-800 p-1 font-fira-mono text-sm leading-6">
