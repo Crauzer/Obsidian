cb05e9dae0be0a62dd51ff2feb028a00e719d1f3
Filip Quitko <filip.quitko@gmail.com>
2024-02-07 16:42:12 +0100

    fix: wad search no results

--- src/features/wad/components/search/WadSearchInput.tsx
+++ src/features/wad/components/search/WadSearchInput.tsx
@@ -1,7 +1,9 @@
 import clsx from 'clsx';
 import { useCombobox } from 'downshift';
 import React, { forwardRef, useState } from 'react';
-import { MdDataArray, MdSearch } from 'react-icons/md';
+import { useTranslation } from 'react-i18next';
+import { FaSearch } from 'react-icons/fa';
+import { MdDataArray } from 'react-icons/md';
 import { useDebounceCallback } from 'usehooks-ts';
 
 import { getLeagueFileKindIcon, getLeagueFileKindIconColor, useSearchWad } from '../..';
@@ -9,67 +11,67 @@ import { Icon, Input, Popover } from '../../../../components';
 
 export type WadSearchInputProps = { wadId: string };
 
-export const WadSearchInput = forwardRef<HTMLInputElement, WadSearchInputProps>(
-  ({ wadId }, ref) => {
-    const [searchQuery, setSearchQuery] = useState('');
-    const handleSearchQueryChange = useDebounceCallback(setSearchQuery, 500);
+export const WadSearchInput: React.FC<WadSearchInputProps> = ({ wadId }) => {
+  const [t] = useTranslation('common');
 
-    const searchWad = useSearchWad({ wadId, query: searchQuery });
+  const [searchQuery, setSearchQuery] = useState('');
+  const handleSearchQueryChange = useDebounceCallback(setSearchQuery, 500);
 
-    const { inputValue, getInputProps, getMenuProps, isOpen, openMenu, closeMenu, getItemProps } =
-      useCombobox({
-        onInputValueChange: (changes) => {
-          handleSearchQueryChange(changes.inputValue ?? '');
-        },
-        itemToString: (item) => item?.path ?? '',
-        items: searchWad.data?.items ?? [],
-      });
+  const searchWad = useSearchWad({ wadId, query: searchQuery });
 
-    return (
-      <Popover.Root
-        open={isOpen}
-        onOpenChange={(open) => {
-          open ? openMenu() : closeMenu();
-        }}
-      >
-        <Popover.Anchor className="flex h-full">
-          <Input
-            {...getInputProps()}
-            ref={ref}
-            className="w-[500px]"
-            placeholder="Search"
-            right={<Icon size="md" icon={MdSearch} />}
-          />
-        </Popover.Anchor>
-        <Popover.Content side="bottom" sideOffset={6} align="end" className="min-w-[500px]">
-          {isOpen && searchWad.isSuccess && searchWad.data.items.length > 0 && (
-            <ul {...getMenuProps()} className="flex flex-col">
-              {isOpen &&
-                inputValue !== '' &&
-                searchWad.data?.items.map((item, index) => (
-                  <li
-                    {...getItemProps({
-                      item,
-                      index,
-                    })}
-                    className="flex flex-row items-center gap-2 rounded p-1 py-2 transition-colors duration-150 hover:cursor-pointer hover:bg-gray-600 hover:shadow-inner"
-                  >
-                    <Icon
-                      className={clsx(getLeagueFileKindIconColor(item.extensionKind))}
-                      icon={getLeagueFileKindIcon(item.extensionKind)}
-                    />
-                    <p className="text-base">{item.path}</p>
-                  </li>
-                ))}
+  const { getInputProps, getMenuProps, isOpen, openMenu, closeMenu, getItemProps } = useCombobox({
+    onInputValueChange: (changes) => {
+      handleSearchQueryChange(changes.inputValue ?? '');
+    },
+    itemToString: (item) => item?.path ?? '',
+    items: searchWad.data?.items ?? [],
+  });
+
+  return (
+    <Popover.Root
+      open={isOpen}
+      onOpenChange={(open) => {
+        open ? openMenu() : closeMenu();
+      }}
+    >
+      <Popover.Anchor className="flex h-full">
+        <Input
+          {...getInputProps()}
+          className="w-[500px]"
+          placeholder="Search"
+          right={<Icon size="md" icon={FaSearch} />}
+        />
+      </Popover.Anchor>
+      <Popover.Content side="bottom" sideOffset={6} align="end" className="min-w-[500px]">
+        {isOpen && (
+          <>
+            {searchWad.data?.items.length === 0 && (
+              <div className="flex flex-col items-center justify-center gap-1">
+                <Icon size="xl" icon={MdDataArray} />
+                <p className="select-none text-base text-gray-50">{t('noResults')}</p>
+              </div>
+            )}
+            <ul {...getMenuProps({}, { suppressRefError: true })} className="flex flex-col">
+              {searchWad.data?.items.map((item, index) => (
+                <li
+                  key={index}
+                  {...getItemProps({
+                    item,
+                    index,
+                  })}
+                  className="flex flex-row items-center gap-2 rounded p-1 py-2 transition-colors duration-150 hover:cursor-pointer hover:bg-gray-600 hover:shadow-inner"
+                >
+                  <Icon
+                    className={clsx(getLeagueFileKindIconColor(item.extensionKind))}
+                    icon={getLeagueFileKindIcon(item.extensionKind)}
+                  />
+                  <p className="text-base">{item.path}</p>
+                </li>
+              ))}
             </ul>
-          )}
-          {isOpen && searchWad.data?.items.length === 0 && (
-            <div className="flex items-center justify-center">
-              <Icon size="xl" icon={MdDataArray} />
-            </div>
-          )}
-        </Popover.Content>
-      </Popover.Root>
-    );
-  },
-);
+          </>
+        )}
+      </Popover.Content>
+    </Popover.Root>
+  );
+};
--- src/i18n/en/common.ts
+++ src/i18n/en/common.ts
@@ -13,4 +13,5 @@ export const common = {
   },
 
   copied: 'Copied!',
+  noResults: 'No results',
 };
